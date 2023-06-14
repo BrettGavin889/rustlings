fc3123c90f469d11032d5f2196ade51e4c44c4fc
Brett Gavin <BrettGavin889@gmail.com>
2023-06-14 13:20:51 -0400

    Commit exercises finished previously

--- exercises/23_conversions/from_into.rs
+++ exercises/23_conversions/from_into.rs
@@ -41,10 +41,26 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
-    fn from(s: &str) -> Person {}
+    //fn from(s: &str) -> Person {}
+    fn from(s: &str) -> Person {
+        if let Some(s_words) = s.split_once(',') {
+            if s_words.0.is_empty() {
+                return Person::default();
+            }
+            let age = match s_words.1.parse::<usize>() {
+                Ok(age)  => age,
+                Err(_) => return Person::default(),
+            };
+            Person {
+                name: s_words.0.to_string(),
+                age,
+            }
+        } else {
+            Person::default()
+        }
+    }
 }
 
 fn main() {
